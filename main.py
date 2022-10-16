59d9416ba29e5be162c5655da9e34aeb78cd4761
pabloBueno <pbueno@unal.edu.co>
2022-10-15 22:08:09 -0500

    implementado movilidad

--- main.py
+++ main.py
@@ -16,9 +16,11 @@ pygame.display.set_icon(icon)
 playerImg = pygame.image.load('player.png')
 playerX = 370
 playerY = 480
+player_change = 0
+playerX_change = 0
 
-def player():
-  screen.blit(playerImg, (playerX, playerY))
+def player(x, y):
+  screen.blit(playerImg, (x, y))
 
 #game loop
 running = True
@@ -30,6 +32,17 @@ while running:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
       running = False
+  #if keystroke is pressed check whether its right or left
+    if event.type == pygame.KEYDOWN:
+      if event.key == pygame.K_LEFT:
+        playerX_change = -0.3
+      if event.key == pygame.K_RIGHT:
+        playerX_change = 0.3     
+    if event.type == pygame.KEYUP:
+      if event.key == pygame.K_LEFT or event.key == pygame.K_RIGHT:
+        playerX_change = 0
 
-  player()
+  #5 = 5 + -0.1 -> 5 = 5 - 0.1        
+  playerX += playerX_change
+  player(playerX, playerY)
   pygame.display.update()    
